565526f81d91ba9944863f790efbd8705e908d22
Tung Pham <tung.42@gmail.com>
2024-12-06 10:54:26 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Container, Navbar } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBook } from "@fortawesome/free-solid-svg-icons"; // Import the book icon
 import BooksSearch from "./BooksSearch";
 
 const App = () => {
@@ -7,7 +9,10 @@ const App = () => {
     <div>
       <Navbar bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand>Tra cứu sách</Navbar.Brand>
+          <Navbar.Brand>
+            <FontAwesomeIcon icon={faBook} className="me-2" /> {/* Book icon */}
+            Tra cứu sách
+          </Navbar.Brand>
         </Container>
       </Navbar>
       <Container className="mt-4">
